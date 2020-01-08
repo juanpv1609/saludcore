6c8df620037478c7210167efc4ce10e2f0b627e0
Juan Perugachi <juanpv1609@gmail.com>
2020-01-08 12:12:24 -0500

    interfazLaboratorio

---
+++ application/controllers/LaboratorioController.php
@@ -0,0 +1,42 @@
+<?php
+
+class LaboratorioController extends Zend_Controller_Action
+{
+
+    public function init()
+    {
+        $this->initView();
+        $this->view->baseUrl = $this->_request->getBaseUrl();
+        $this->view->user = Zend_Auth::getInstance()->getIdentity();
+    }
+
+    public function indexAction()
+    {
+        // action body
+    }
+
+    public function preDispatch()
+    {
+        $auth = Zend_Auth::getInstance();
+          if (!$auth->hasIdentity()) {
+           $this->_redirect('login/index');
+          }
+          /*else if($auth->hasIdentity()) {
+            $user = $auth->getIdentity();
+              if( $user->id_perfil == 4){
+                Zend_Auth::getInstance()->clearIdentity();
+                $this->_redirect('auth/login/e/0');
+              }
+           }*/
+    }
+
+    public function pedidoAction()
+    {
+        // action body
+    }
+
+
+}
+
+
+
